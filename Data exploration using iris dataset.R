d15f9ad598b46134f506b5f833162b84540e6287
JackRM10 <jrmclean10@gmail.com>
2024-10-10 11:40:09 -0700

    Create Data exploration using iris dataset.R
    Using the built-in iris dataset in R, data exploration performed by calculating summary statistics and creating a scatter plot of sepal length and width.

---
+++ Data exploration using iris dataset.R
@@ -0,0 +1,18 @@
+# Challenge 5-------------------------------------------------------------------
+# Load the iris dataset
+data(iris)
+
+# Summary statistics for the iris dataset
+summary(iris)
+
+# Scatter plot of Sepal Length vs Sepal Width
+plot(iris$Sepal.Length, iris$Sepal.Width,
+     main = "Scatter plot of Sepal Length vs Sepal Width",
+     xlab = "Sepal Length",
+     ylab = "Sepal Width",
+     col = iris$Species,  # Color points by species
+     pch = 19)            # Use filled circles for points
+
+# Add a legend to the plot
+legend("topright", legend = levels(iris$Species), col = 1:3, pch = 19)
+#-------------------------------------------------------------------------------
